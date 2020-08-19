0cda50db739b12cf0fd94873b4f423893212979e
Pásztor Bertalan <pasztor.bertalan@autsoft.hu>
2020-08-19 14:34:51 +0200

    Bugs fixes.

--- board.js
+++ board.js
@@ -8,24 +8,24 @@ document.addEventListener('DOMContentLoaded', () => {
     var windowWidth = window.innerWidth;
     var windowHeight = window.innerHeight;
 
-    var maxSize = Math.min(windowWidth, windowHeight) - 20;
-    squareSize = Math.floor((maxSize-8) / 8);
+    var maxSize = Math.min(windowWidth, windowHeight) - 10;
+    squareSize = Math.floor((maxSize -8 ) / 8);
 
-    board.style.width = maxSize + "px";
-    board.style.height = maxSize + "px";
+    board.style.width = squareSize * 8 + "px";
+    board.style.height = squareSize * 8 + "px";
     for(let i = 0; i < width*height; i++) {
         let square = document.createElement('div');
         square.setAttribute('id', i);
         square.style.width = squareSize + 'px';
         square.style.height = squareSize + 'px';
-        /*if ((Math.floor(i/width))%2 == 0) {
+        if ((Math.floor(i/width))%2 == 0) {
             if (i%2 == 0)
                 square.setAttribute('class', 'black')
         }
         else {
             if (i%2 != 0)
                 square.setAttribute('class', 'black')
-        }*/
+        }
         board.appendChild(square)
         squares.push(square);     
     }
